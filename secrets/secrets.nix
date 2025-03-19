0e61f177d7ee438684955dbba1f69d9a1da2d6a1
somura <somura-vanilla@so-icecream.com>
2025-03-19 17:53:55 +0900

    fix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   somura_chocolate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpLknzjjRI5ocXsoPC9xKCqmVmq17ddgEOqogxZYPPI";
   users = [ somura_vanilla somura_chocolate ];
 
-  vanilla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHy9skF2XWJ6eJ5lx9OpiL60ExkgQuh1OqpEbSM7UT32";
+  vanilla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM3pXzq/BAoWhdd7xcww1MvFdI0B+QDBBkkVvi47sSsY";
   chocolate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAm7YMm9zfTuPs1U9tEPhR07/Rl0Fer4LEjrltCFK7Xo";
   systems = [ vanilla chocolate ];
 in
