94c43c017db22f1b6eb8fdfd6eb4342c7849d7c3
somura <somura-vanilla@so-icecream.com>
2025-03-19 15:04:46 +0900

    add new machine key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
+  somura_vanilla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN06dm12KnxrWJ6AAjyjLHXYbvjxL26QR9xZlfKeL6bO";
   somura_chocolate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpLknzjjRI5ocXsoPC9xKCqmVmq17ddgEOqogxZYPPI";
-  users = [ somura_chocolate ];
+  users = [ somura_vanilla somura_chocolate ];
 
+  vanilla = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHy9skF2XWJ6eJ5lx9OpiL60ExkgQuh1OqpEbSM7UT32";
   chocolate = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAm7YMm9zfTuPs1U9tEPhR07/Rl0Fer4LEjrltCFK7Xo";
-  systems = [ chocolate ];
+  systems = [ vanilla chocolate ];
 in
 {
   "general.fish.age".publicKeys = users ++ systems;
