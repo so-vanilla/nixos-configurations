29e1eea7a519d8ec75457d53b574a7eb07c7470d
somura <somura-vanilla@so-icecream.com>
2025-04-19 17:33:27 +0900

    add nvim-autopairs

--- home-manager/programs/neovim/nvim/lua/plugin/init.lua
+++ home-manager/programs/neovim/nvim/lua/plugin/init.lua
@@ -3,6 +3,7 @@ require('lazy').setup({
         require('plugin.nvim-lspconfig'),
         require('plugin.nvim-cmp'),
         require('plugin.telescope-nvim'),
+        require('plugin.nvim-autopairs'),
         require('plugin.hop-nvim'),
         require('plugin.neogit'),
         require('plugin.catppuccin'),
