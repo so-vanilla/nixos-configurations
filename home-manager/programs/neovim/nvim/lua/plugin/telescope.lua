58c7ecee0f76f7694a8953e2e13e57161d89d8d1
somura <somura-vanilla@so-icecream.com>
2025-04-27 12:51:40 +0900

    add keymap

--- home-manager/programs/neovim/nvim/lua/plugin/telescope.lua
+++ home-manager/programs/neovim/nvim/lua/plugin/telescope.lua
@@ -8,5 +8,6 @@ return {
       { '<M-x>', function () require('telescope.builtin').commands() end },
       { '<C-s>', function () require('telescope.builtin').current_buffer_fuzzy_find() end },
       { '<C-x>b', function () require('telescope.builtin').buffers() end },
+      { '<C-x><C-f>', function () require('telescope.builtin').find_files() end },
     },
 }
