2401007d2520de52b2217da6ba1ffe06529717fd
somura <somura-vanilla@so-icecream.com>
2025-04-27 20:38:27 +0900

    format

--- home-manager/programs/neovim/nvim/lua/core/opts/init.lua
+++ home-manager/programs/neovim/nvim/lua/core/opts/init.lua
@@ -1,11 +1,12 @@
 local opts = {
-   shiftwidth = 2,
-   tabstop = 2,
-   expandtab = true,
-   smartindent = true,
-   autoindent = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  expandtab = true,
+  smartindent = true,
+  autoindent = true,
+
 }
 
 for k, v in pairs(opts) do
-   vim.opt[k] = v
+  vim.opt[k] = v
 end
