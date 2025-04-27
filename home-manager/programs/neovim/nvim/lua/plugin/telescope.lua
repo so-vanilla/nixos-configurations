92e8d97080b915492799ec12f3e8c3bce86bb3eb
somura <somura-vanilla@so-icecream.com>
2025-04-27 20:36:05 +0900

    add file-browser extension

--- home-manager/programs/neovim/nvim/lua/plugin/telescope.lua
+++ home-manager/programs/neovim/nvim/lua/plugin/telescope.lua
@@ -1,13 +1,14 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    cmd = 'Telescope',
-    keys = {
-      { '<M-x>', function () require('telescope.builtin').commands() end },
-      { '<C-s>', function () require('telescope.builtin').current_buffer_fuzzy_find() end },
-      { '<C-x>b', function () require('telescope.builtin').buffers() end },
-      { '<C-x><C-f>', function () require('telescope.builtin').find_files() end },
-    },
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  cmd = 'Telescope',
+  keys = {
+    { '<M-x>', function () require('telescope.builtin').commands() end },
+    { '<C-s>', function () require('telescope.builtin').current_buffer_fuzzy_find() end },
+    { '<C-x>b', function () require('telescope.builtin').buffers() end },
+    { '<C-x><C-f>', function () require('telescope').extensions.file_browser.file_browser() end },
+  },
 }
