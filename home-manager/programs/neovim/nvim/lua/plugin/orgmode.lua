1ee6c6c654db47c682c62f21603ba6eb20d8751b
somura <somura-vanilla@so-icecream.com>
2025-04-28 23:28:16 +0900

    change keymap

--- home-manager/programs/neovim/nvim/lua/plugin/orgmode.lua
+++ home-manager/programs/neovim/nvim/lua/plugin/orgmode.lua
@@ -1,12 +1,18 @@
 return {
   'nvim-orgmode/orgmode',
-  cmd = { 'Org' },
-  ft = { 'org' },
   opts = {
     org_agenda_files = '~/org/*',
     org_adapt_indentation = false,
+    org_startup_folded = 'showeverything',
     mappings = {
-      disable_all = true,
+      global = {
+        org_agenda = '<F2>a',
+        org_capture = '<F2>c',
+      },
+      org = {
+        org_timestamp_up = false,
+        org_timestamp_down = false,
+      }
     },
   }
 }
