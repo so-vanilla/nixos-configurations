2ed0872082153b3530bf4592db7a9d96a3992fd5
somura <somura-vanilla@so-icecream.com>
2025-05-25 10:31:58 +0900

    add firewall/default.nix example

--- hosts/ThinkPadX13/networkig/default.nix
+++ hosts/ThinkPadX13/networkig/default.nix
@@ -1,7 +1,11 @@
 { pkgs, hostname, ... }:
 let
   networkmanager = import ./networkmanager { inherit pkgs; };
-  firewall = ./firewall;
+  firewall =
+    if builtins.pathExists ./firewall/default.nix then
+      ./firewall
+    else
+      abort "hosts/.../firewall/default.nix is not exist. Make it.";
 in
 [
   {
