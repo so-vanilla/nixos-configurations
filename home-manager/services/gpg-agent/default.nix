fc2cc7d99ce2ab07cd74b85e2c797a9c84131810
somura <somura-vanilla@so-icecream.com>
2025-06-08 19:46:41 +0900

    change ttl

--- home-manager/services/gpg-agent/default.nix
+++ home-manager/services/gpg-agent/default.nix
@@ -9,6 +9,8 @@
       allow-emacs-pinentry
       allow-loopback-pinentry
     '';
+    defaultCacheTtl = 315360000;
+    maxCacheTtl = 315360000;
     pinentry = {
       package = pkgs.pinentry-curses;
       program = "pinentry-curses";
