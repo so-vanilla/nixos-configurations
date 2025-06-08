8206d6ba57c2d75091084b40789b0828346c4bb1
somura <somura-vanilla@so-icecream.com>
2025-06-08 20:08:12 +0900

    REMOVE FOOLISH CONFIG

--- home-manager/services/gpg-agent/default.nix
+++ home-manager/services/gpg-agent/default.nix
@@ -9,8 +9,6 @@
       allow-emacs-pinentry
       allow-loopback-pinentry
     '';
-    defaultCacheTtl = 315360000;
-    maxCacheTtl = 315360000;
     pinentry = {
       package = pkgs.pinentry-curses;
       program = "pinentry-curses";
