fe5580364999e3d01cf97c87664a7091d156c6a1
somura <somura-vanilla@so-icecream.com>
2025-06-11 20:07:11 +0900

    add lsp-mode settings

---
+++ home-manager/programs/emacs/early-init.el
@@ -0,0 +1,13 @@
+;;; early-init.el --- My early-init.el
+
+;; Author: Shuto Omura <somura-vanilla@so-icecream.com>
+
+;; Commentary:
+
+;;; Code:
+
+(setenv "LSP_USE_PLISTS" "true")
+
+(provide 'early-init)
+
+;;; early-init.el ends here
