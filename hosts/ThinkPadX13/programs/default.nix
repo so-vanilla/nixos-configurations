6c19aaf932a970b6bf5dd230fe35d67a0fce54d8
somura <somura-vanilla@so-icecream.com>
2025-06-22 12:01:44 +0900

    add virt-manager

--- hosts/ThinkPadX13/programs/default.nix
+++ hosts/ThinkPadX13/programs/default.nix
@@ -3,9 +3,11 @@ let
   hyprland = import ./hyprland;
   wireshark = import ./wireshark { inherit pkgs; };
   steam = import ./steam { inherit pkgs; };
+  virt-manager = import ./virt-manager;
 in
 [
   hyprland
   wireshark
   steam
+  virt-manager
 ]
