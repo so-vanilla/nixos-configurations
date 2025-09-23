a98ca9e927e87e72170453aec3cd39d8bfc2a60f
somura <somura-vanilla@so-icecream.com>
2025-09-23 10:46:20 +0900

    snapshot 20250923

--- hosts/ThinkPadX13/services/displayManager/sddm/default.nix
+++ hosts/ThinkPadX13/services/displayManager/sddm/default.nix
@@ -2,7 +2,7 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "catppuccin-latte";
+    # theme = "catppuccin-latte";
   };
   catppuccin.sddm = {
     enable = true;
