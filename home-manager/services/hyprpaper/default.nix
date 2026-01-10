4883b6436f7e484b6815e78a4f245f484062c91c
somura <somura-vanilla@so-icecream.com>
2026-01-10 11:02:11 +0900

    変更に追従

--- home-manager/services/hyprpaper/default.nix
+++ home-manager/services/hyprpaper/default.nix
@@ -7,17 +7,22 @@
           "~/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png"
         ];
         wallpaper = [
-          # for ThinkPad X13
-          # builtin HDMI port
-          "HDMI-A-1, /home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png"
-
-          # for ENVYx360-13
-          # builtin monitor
-          "eDP-1, /home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png"
-          # USB Type-C/1 to HDMI adapter
-          "DP-1, /home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png"
-          # USB Type-C/2 to HDMI adapter
-          "DP-2, /home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png"
+          {
+            monitor = "eDP-1";
+            path = "/home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png";
+          }
+          {
+            monitor = "DP-1";
+            path = "/home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png";
+          }
+          {
+            monitor = "DP-2";
+            path = "/home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png";
+          }
+          {
+            monitor = "HDMI-A-1";
+            path = "/home/somura/repos/github.com/NixOS/nixos-artwork/wallpapers/nix-wallpaper-nineish.png";
+          }
         ];
       };
     };
