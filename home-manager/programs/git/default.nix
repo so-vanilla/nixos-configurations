40407837d8e0290dd21428bc28a373a267b44d3d
somura <shuto-vanilla@so-icecream.com>
2026-06-15 10:14:39 +0900

    Use gh credential helper on Darwin

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -2,28 +2,45 @@
   pkgs,
   email,
 }:
+let
+  credentialSettings =
+    if pkgs.stdenv.isDarwin then
+      {
+        credential = {
+          helper = "";
+        };
+        "credential \"https://github.com\"" = {
+          helper = "!gh auth git-credential";
+        };
+      }
+    else
+      {
+        credential = {
+          helper = "store";
+        };
+      };
+in
 {
   programs.git = {
     enable = true;
-    ignores =
-      [
-        ".claude"
-        "claude-plans"
-        "result"
-      ]
-      ++ (
-        if pkgs.stdenv.isDarwin then
-          [
-            ".envrc"
-            ".dir-locals.el"
-            "devenv.nix"
-            "devenv.yaml"
-            "devenv.lock"
-            ".devenv.flake.nix"
-          ]
-        else
-          [ ]
-      );
+    ignores = [
+      ".claude"
+      "claude-plans"
+      "result"
+    ]
+    ++ (
+      if pkgs.stdenv.isDarwin then
+        [
+          ".envrc"
+          ".dir-locals.el"
+          "devenv.nix"
+          "devenv.yaml"
+          "devenv.lock"
+          ".devenv.flake.nix"
+        ]
+      else
+        [ ]
+    );
     settings = {
       core = {
         editor = "nvim";
@@ -36,15 +53,13 @@
       init = {
         defaultbranch = "main";
       };
-      credential = {
-        helper = "store";
-      };
       github = {
         user = "so-vanilla";
       };
       ghq = {
         root = "~/repos";
       };
-    };
+    }
+    // credentialSettings;
   };
 }
